df3d013c2023cd33f3c85787e1a1d4e4213c5cbf
kramduckner <buckner.a.mark@gmail.com>
2017-10-05 10:42:17 -0600

    added all the icon stuff

--- src/iconWidget/IconWidget.js
+++ src/iconWidget/IconWidget.js
@@ -1,3 +1,6 @@
+import React, { Component } from 'react';
+var FontAwesome = require('react-fontawesome');
+
 // This exercise is getting building a component different from the weather table
 // that will utilize the api data. Based on weather description found in the 
 // api response, render the following icons:
@@ -5,3 +8,47 @@
 //  http://fontawesome.io/icon/cloud/
 //  http://fontawesome.io/icon/sun-o/
 // Display them next to weather description, and put them inside a react-strap card.
+// In the title of the card, display the city. 
+
+// tips:
+//  First, lets get the react-strap card working. Go to https://reactstrap.github.io/components/card/,
+//  copy and paste their example into your render() method.Put some 
+//  sample data inside and mess around with it to get a feel how it works. 
+//  Next, get the proper props passed in, the weather data through the weather API.
+//  Next, let's get font awesome working - I've imported the library "react-fontawesome"
+//  Go to the readme located at https://github.com/danawoodman/react-fontawesome
+//  to see some ways in which it is used. 
+//  Here's an example of how I used the card and the fontawesome libs:
+//  <Card>
+//    <CardBody>
+//     <CardTitle>{title}</CardTitle>
+//       <CardSubtitle>Clear sky</CardSubtitle>
+//       <FontAwesome
+//         className ="{font awesome classes}"
+//         name="{name}"
+//         size="2x"
+//        />
+//      </CardBody>
+//    </Card>   
+// So now get it working to render the proper font-awesome icon! There's many
+// ways to do this. 
+
+class IconWidget extends Component {
+  render() {
+    return (
+      <Card>
+      <CardBody>
+        <CardTitle>Bozeman</CardTitle>
+        <CardSubtitle>Clear sky</CardSubtitle>
+         <FontAwesome
+          className ="fa fa-sun-o"
+          name="umbrella"
+          size="2x"
+      />
+      </CardBody>
+    </Card>   
+
+    );
+  }
+}
+export default IconWidget;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import WeatherComponent from "./Weather.js";
 import 'bootstrap/dist/css/bootstrap.css';
-
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
 ReactDOM.render(<WeatherComponent />, document.getElementById('root'));
 
