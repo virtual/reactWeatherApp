7bc122f52d9939896f99200b08a06203d3f6b858
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-10-06 14:47:02 -0600

    added weather updates and searchbox

--- src/iconWidget/IconWidget.js
+++ src/iconWidget/IconWidget.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button } from 'reactstrap';
+import SearchBar from '../searchBar/SearchBar';
+import "./IconWidget.css";
 var FontAwesome = require('react-fontawesome');
-
-
 
 // This exercise is getting building a component different from the weather table
 // that will utilize the api data. Based on weather description found in the 
@@ -42,34 +42,50 @@ var FontAwesome = require('react-fontawesome');
 // Here's a screenshot of what mine looks like. (It's stand alone, no table in it yet)
 
 class WeatherComponent extends React.Component {
-  constructor() {
+  constructor () {
     super();
     this.state = {
       initialized: false
     };
+    this.updateClick = this.updateClick.bind(this);
   }
 
-  componentDidMount() {
+  updateClick (city) {
+   console.log(city)
+   this.setState({
+    initialized: false
+   })
+    //console.log('uc!', this);
+    // wrap your
+    // logic fetching all the weather api data into a method.
     var jeanineapi = 'e6ea27b1c535e375f2f3ab9cfeab7df6';
     var markapi = '3d6b633422451393e953dab4052ea0e4';
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=' + jeanineapi;
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + markapi;
     fetch(url).then(function (response) {
       return response.json();
     }).then((weatherObj) => {
-      //console.log(weatherObj)
-      this.weatherData = weatherObj;
-      this.setState({
-        initialized: true
-      });
+      if (weatherObj !== undefined ) {
+      // console.log(weatherObj)
+        this.weatherData = weatherObj;
+        this.setState({
+          initialized: true
+        });
+      } else {
+        this.updateClick();
+      }
     });
   }
+  componentDidMount () {
+    this.updateClick("Bozeman");
+  }
 
-  render() {
+  render () {
     if (this.state.initialized) {
       return (
         <div>
-          <h1>{this.weatherData.name}</h1>
+          <h1>Weather</h1>
           <IconWidget weatherData={this.weatherData} />
+          <SearchBar city={this.weatherData.name} updateClick={this.updateClick} />
         </div>
       );
     } else {
@@ -82,55 +98,54 @@ class WeatherComponent extends React.Component {
   }
 }
 class IconWidget extends Component {
-
-
-  render() {
-    //console.log(this.props);
+  render () {
+     console.log(this.props);
     return (
       <Card>
-      <CardBody>
-        <CardTitle>{this.props.weatherData.name}</CardTitle>
-        <CardSubtitle>{this.props.weatherData.weather[0].description}</CardSubtitle>
-        <WeatherIcon desc={this.props.weatherData.weather[0].description}/>
-      </CardBody>
-    </Card>   
+        <CardBody>
+          <CardTitle>{this.props.weatherData.name}</CardTitle>
+          <CardTitle>{this.props.weatherData.main.temp}&deg;F</CardTitle>
+          <CardSubtitle>{this.props.weatherData.weather[0].description}</CardSubtitle>
+          <WeatherIcon desc={this.props.weatherData.weather[0].description} />
+        </CardBody>
+      </Card>
 
     );
   }
 }
 class WeatherIcon extends Component {
-  constructor() {
+  constructor () {
     super();
   }
-  render() {
-    console.log(this.props);
-    var rainArr = ["thunderstorm with light rain","thunderstorm with rain","thunderstorm with heavy rain","light thunderstorm","thunderstorm","heavy thunderstorm","ragged thunderstorm","thunderstorm with light drizzle","thunderstorm with drizzle","thunderstorm with heavy drizzle","light intensity drizzle","drizzle","heavy intensity drizzle","light intensity drizzle rain","drizzle rain","heavy intensity drizzle rain","shower rain and drizzle","heavy shower rain and drizzle","shower drizzle","light rain","moderate rain","heavy intensity rain","very heavy rain","extreme rain","freezing rain","light intensity shower rain","shower rain","heavy intensity shower rain","ragged shower rain","light snow","snow","heavy snow","sleet","shower sleet","light rain and snow","rain and snow","light shower snow","shower snow","heavy shower snow","mist","smoke","haze","sand, dust whirls","fog","sand","dust","volcanic ash","squalls","tornado"];
-    var clearArr = ["clear sky","calm","light breeze","gentle breeze","moderate breeze","fresh breeze"];
-    var cloudArr = ["few clouds","scattered clouds","broken clouds","overcast clouds","tornado","tropical storm","hurricane","cold","hot","windy","hail","strong breeze","high wind, near gale","gale","severe gale","storm","violent storm","hurricane"];
-    let icon = "sun-o";
-    //check what desc is and use matching icon
+  render () {
+    // console.log(this.props);
+    var rainArr = ['thunderstorm with light rain', 'thunderstorm with rain', 'thunderstorm with heavy rain', 'light thunderstorm', 'thunderstorm', 'heavy thunderstorm', 'ragged thunderstorm', 'thunderstorm with light drizzle', 'thunderstorm with drizzle', 'thunderstorm with heavy drizzle', 'light intensity drizzle', 'drizzle', 'heavy intensity drizzle', 'light intensity drizzle rain', 'drizzle rain', 'heavy intensity drizzle rain', 'shower rain and drizzle', 'heavy shower rain and drizzle', 'shower drizzle', 'light rain', 'moderate rain', 'heavy intensity rain', 'very heavy rain', 'extreme rain', 'freezing rain', 'light intensity shower rain', 'shower rain', 'heavy intensity shower rain', 'ragged shower rain', 'light snow', 'snow', 'heavy snow', 'sleet', 'shower sleet', 'light rain and snow', 'rain and snow', 'light shower snow', 'shower snow', 'heavy shower snow', 'mist', 'smoke', 'haze', 'sand, dust whirls', 'fog', 'sand', 'dust', 'volcanic ash', 'squalls', 'tornado'];
+    var clearArr = ['clear sky', 'calm', 'light breeze', 'gentle breeze', 'moderate breeze', 'fresh breeze'];
+    var cloudArr = ['few clouds', 'scattered clouds', 'broken clouds', 'overcast clouds', 'tornado', 'tropical storm', 'hurricane', 'cold', 'hot', 'windy', 'hail', 'strong breeze', 'high wind, near gale', 'gale', 'severe gale', 'storm', 'violent storm', 'hurricane'];
+    let icon = 'sun-o';
+    // check what desc is and use matching icon
     var description = this.props.desc;
-    if (rainArr.indexOf(description) !== -1){
-      icon = "umbrella";
+    if (rainArr.indexOf(description) !== -1) {
+      icon = 'umbrella';
     } else if (cloudArr.indexOf(description) !== -1) {
-      icon = "cloud";
+      icon = 'cloud';
     }
-    return(
+    return (
       <div>
         <FontAwesome
           name={icon}
-          size="4x"
+          size='4x'
         />
       </div>
-    )
+    );
   }
 }
 class ConvertToF extends Component {
-  constructor() {
+  constructor () {
     super();
   }
-  render() {
-    //T(°F) = T(K) × 9/5 - 459.67
+  render () {
+    // T(°F) = T(K) × 9/5 - 459.67
     var tempK = this.props.tempK;
     var farTemp = Math.round(tempK * 9 / 5 - 459.67);
     return (
--- src/searchBar/SearchBar.js
+++ src/searchBar/SearchBar.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
+import { Button, InputGroup, InputGroupAddon, Input } from 'reactstrap';
+import WeatherComponent from '../iconWidget/IconWidget.js';
 var FontAwesome = require('react-fontawesome');
 
+// So hopefully we have a bunch of components and our 'state' is being managed in only
+// one of them (the 'top' most, the one making the api call). Now lets build a 
+// new component that has an <input> and <button> that can be clicked. Use
+// react strap for the button and input. Now go to the 'top' most component and wrap your
+// logic fetching all the weather api data into a method. Pass that method down
+// into this new component and call it when the user clicks on the button. 
 
-//So hopefully we have a bunch of components and our 'state' is being managed in only
-//one of them (the 'top' most, the one making the api call). Now lets build a 
-//new component that has an <input> and <button> that can be clicked. Use
-//react strap for the button and input. Now go to the 'top' most component and wrap your
-//logic fetching all the weather api data into a method. Pass that method down
-//into this new component and call it when the user clicks on the button. 
+// This function will construct a new url string where you inject the city into
+// the proper place (where bozeman is, look at the url var in Weather.js)
+// Use state in this function to automatically update the table and iconWidget
+// to reflect the changes. 
 
-//This function will construct a new url string where you inject the city into
-//the proper place (where bozeman is, look at the url var in Weather.js)
-//Use state in this function to automatically update the table and iconWidget
-//to reflect the changes. 
+// You've pretty much a react app!!!!
 
-//You've pretty much a react app!!!!
+class SearchBar extends Component {
+  constructor () {
+    super();
+    this.state = {
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-class SearchBar extends Component{
+  handleChange (event) {
+    console.log('handle change called');
+   this.setState({value: event.target.value});
+  }
 
+  handleClick() {
+   // console.log('helloooooo')
+   // console.log(this.state.value)
+   if (this.state.value !== '') {
+    this.props.updateClick(this.state.value);
+   }
+  }
+
+
+  render () {
+    //console.log(this.props);
+    return (
+      <div className="cityform">
+        <InputGroup>
+          <Input placeholder='City Name' value={this.state.value} onChange={this.handleChange} />
+        </InputGroup>
+        <Button onClick={this.handleClick} color='primary'>Update</Button>
+      </div>
+    );
+  }
 }
+
+export default SearchBar;
